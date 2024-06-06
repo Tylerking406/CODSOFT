9daf1c4c0ed3a6776ed717fb8ecd90555f850841
Arinao Ndou <tylerking406@gmail.com>
2024-06-06 05:58:41 +0200

    I implemented Attempt feature

--- numberGame.java
+++ numberGame.java
@@ -1,3 +1,4 @@
+package CODSOFT;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,34 +12,34 @@ public class numberGame {
         
         //Allow user to input a number
         System.out.print("Enter your guess: ");
-        Scanner input = new Scanner(System.in);
-        int guess= input.nextInt();
+        try (Scanner input = new Scanner(System.in)) {
+            int guess= input.nextInt();
 
-        //generate random number between 1 and 100
-        Random random = new Random();
-        int randomNumber = random.nextInt(100)+1; //0 to 99 shifted by 1
+            //generate random number between 1 and 100
+            Random random = new Random();
+            int randomNumber = random.nextInt(100)+1; //0 to 99 shifted by 1
 
-        while(guess != randomNumber){
-            if (guess> randomNumber) {
-                System.out.println("The guess is too high");
+            int attempts = 5; //try agains
 
-                System.out.println("Enter your guess again:");
-                guess= input.nextInt();
-            }
-    
-            else if( guess < randomNumber){
-                System.out.println("The guess is too low");
-                System.out.println("Enter your guess again:");
-                guess= input.nextInt();
-            }
-            else{
+            while(guess != randomNumber && (attempts>1)){
+                attempts--;  //decrement attempt
+                if (guess> randomNumber) {
+                    System.out.println("The guess is too high, please try again:");
+                    guess= input.nextInt();
+                }
+   
+                else if( guess < randomNumber){
+                    System.out.println("The guess is too low, please try again:");
+                    guess= input.nextInt();
+                }
+   
+            }   
+            
+            if (guess == randomNumber) 
                 System.out.println("The guess is correct!!");
-                System.out.println("Enter your guess again:");
-                guess= input.nextInt();
-    
-            }
+            else
+                System.out.println("You run out of attempts");
         }
-
 
     }
 }
